9fd2822fa554d2e704c1b6dff29735c062840c20
Umut Yerebakmaz <umutyerebakmaz@gmail.com>
2022-04-21 21:06:35 +0300

    Add button styles

--- libs/button/src/lib/components/button/button.component.stories.ts
+++ libs/button/src/lib/components/button/button.component.stories.ts
@@ -1,51 +1,14 @@
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
 import { BioButtonModule } from '../../button.module';
-import { BioButtonComponent } from './button.component';
+
 
 export default {
-  title: 'BioButtonComponent',
-  component: BioButtonComponent,
-  decorators: [
-    moduleMetadata({
-      imports: [BioButtonModule],
-    })
-  ],
-} as Meta<BioButtonComponent>;
+  title: 'Button',
+}
 
-const Template: Story<BioButtonComponent> = (args: BioButtonComponent) => ({
-  props: args,
+export const primary = () => ({
+  moduleMetadata: {
+    imports: [BioButtonModule],
+  },
+  template: `<button bio-button type="button">Primary</button>`,
+  props: {}
 });
-
-
-export const Primary = Template.bind({});
-Primary.args = {
-  color: 'primary'
-}
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  color: 'secondary'
-}
-
-export const White = Template.bind({});
-White.args = {
-  color: 'white'
-}
-
-export const LeadingIcon = Template.bind({});
-LeadingIcon.args = {
-}
-
-
-export const TrailingIcon = Template.bind({});
-TrailingIcon.args = {
-}
-
-export const Round = Template.bind({});
-Round.args = {
-}
-
-
-export const Circular = Template.bind({});
-Circular.args = {
-}
--- libs/button/src/lib/components/button/button.component.ts
+++ libs/button/src/lib/components/button/button.component.ts
@@ -1,12 +1,37 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy, Input, ElementRef, OnInit, OnDestroy } from '@angular/core';
+import { FocusMonitor } from '@angular/cdk/a11y';
 @Component({
   selector: '[bio-button]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.bio-btn-primary]': 'color === "primary"',
+    '[class.bio-btn-secondary]': 'color === "secondary"',
+    '[class.bio-btn-white]': 'color === "white"',
+    '[class.xs-btn]': 'size === "xs"',
+    '[class.sm-btn]': 'size === "sm"',
+    '[class.md-btn]': 'size === "md"',
+    '[class.lg-btn]': 'size === "lg"',
+    '[class.xl-btn]': 'size === "xl"',
+  }
 })
-export class BioButtonComponent {
+export class BioButtonComponent implements OnInit, OnDestroy {
   @Input() color!: 'primary' | 'secondary' | 'white';
+  @Input() size!: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  @Input() icon!: 'leading' | 'trailing';
   @Input() disabled!: boolean;
+
+  constructor(
+    private readonly elementRef: ElementRef<HTMLButtonElement>,
+    private readonly focusMonitor: FocusMonitor
+  ) {}
+
+  ngOnInit(): void {
+    this.focusMonitor.monitor(this.elementRef);
+  }
+
+  ngOnDestroy(): void {
+    this.focusMonitor.stopMonitoring(this.elementRef);
+  }
 }
