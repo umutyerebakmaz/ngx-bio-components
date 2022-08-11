f279fca78d975a79793ce8aac5e689d3fcf8613c
umutyerebakmaz <umutyerebakmaz@gmail.com>
2022-08-12 00:30:23 +0300

    add class conditions

--- libs/avatar/src/lib/components/avatar/avatar.component.stories.ts
+++ libs/avatar/src/lib/components/avatar/avatar.component.stories.ts
@@ -11,8 +11,19 @@ const Template: Story<BioAvatarComponent> = (args: BioAvatarComponent) => ({
         imports: [BioAvatarModule],
     },
     template: `<bio-avatar [avatar]="avatar">
-<img #imgElement *ngIf="avatar.img" class="circular xs" [src]="avatar.img" atr="23"/>
-<span #spanElement class="badge"></span>
+<img
+[ngClass]="{
+'avatar-image': true,
+'circular': avatar.shape === 'circular',
+'rounded': avatar.shape === 'rounded',
+'xxs': avatar.size === 'xxs',
+'xs': avatar.size === 'xs',
+'sm': avatar.size === 'sm',
+'md': avatar.size === 'md',
+'lg': avatar.size === 'lg',
+'xl': avatar.size === 'xl'
+}" *ngIf="avatar.img" [src]="avatar.img"/>
+<span></span>
 </bio-avatar>
 `,
     props: {
@@ -23,7 +34,8 @@ const Template: Story<BioAvatarComponent> = (args: BioAvatarComponent) => ({
 export const Primary = Template.bind({});
 Primary.args = {
     avatar: {
-        size: 'xl',
+        shape: 'circular',
+        size: 'xxs',
         disabled: true,
         img: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
     },
--- libs/avatar/src/lib/components/avatar/avatar.component.ts
+++ libs/avatar/src/lib/components/avatar/avatar.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, ChangeDetectionStrategy, ElementRef, Input, SimpleChanges, Renderer2, ViewChild } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
 export type Avatar = {
-    shape?: 'circular' | 'rounded';
-    size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-    badgePosition?: 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRigh';
-    badgeColor?: 'gray' | 'red' | 'green';
-    img?: string,
-    alt?: string,
-    placeholder?: string;
-    disabled?: boolean,
+    shape: 'circular' | 'rounded';
+    size: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+    badgePosition: 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRigh';
+    badgeColor: 'gray' | 'red' | 'green';
+    img: string,
+    alt: string,
+    placeholder: string;
+    disabled: boolean,
 };
 
 @Component({
@@ -17,31 +17,6 @@ export type Avatar = {
     styleUrls: ['./avatar.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BioAvatarComponent implements OnInit {
-
+export class BioAvatarComponent {
     @Input() avatar!: Avatar;
-    imgElement!: HTMLCollectionOf<HTMLImageElement>;
-    spanElement!: HTMLCollectionOf<HTMLSpanElement>;
-
-    constructor(
-        private readonly elementRef: ElementRef<HTMLElement>,
-        private renderer: Renderer2,
-    ) {}
-
-    ngOnInit(): void {
-        this.imgElement = this.elementRef.nativeElement?.getElementsByTagName('img');
-        this.spanElement = this.elementRef.nativeElement?.getElementsByTagName('span');
-        console.log(this.imgElement);
-        console.log(this.spanElement);
-        this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', '#000');
-        this.renderer.setStyle(this.elementRef.nativeElement, 'display', 'inline-flex');
-    }
-
-    // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method, @typescript-eslint/no-unused-vars, @angular-eslint/use-lifecycle-interface
-    ngOnChanges(changes: SimpleChanges): void {
-        //
-
-    }
-
-
 }
