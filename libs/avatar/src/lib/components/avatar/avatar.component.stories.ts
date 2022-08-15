e77f318533ae0cb83a0233686dcbc25783dbd795
umutyerebakmaz <umutyerebakmaz@gmail.com>
2022-08-15 16:08:36 +0300

    add avatar placeholder feature

--- libs/avatar/src/lib/components/avatar/avatar.component.stories.ts
+++ libs/avatar/src/lib/components/avatar/avatar.component.stories.ts
@@ -10,68 +10,21 @@ const Template: Story<BioAvatarComponent> = (args: BioAvatarComponent) => ({
     moduleMetadata: {
         imports: [BioAvatarModule],
     },
-    // buradaki yazılış son kullanım şekli . yayınlamadan önce
-    // <div .... diye html olsun
-    template: `<bio-avatar [avatar]="avatar" [ngClass]="{'default': true}">
-<img
-[ngClass]="{
-'avatar-image': true,
-'circular': avatar.shape === 'circular',
-'rounded': avatar.shape === 'rounded',
-'xxs': avatar.size === 'xxs',
-'xs': avatar.size === 'xs',
-'sm': avatar.size === 'sm',
-'md': avatar.size === 'md',
-'lg': avatar.size === 'lg',
-'xl': avatar.size === 'xl'
-}" *ngIf="avatar.img; else icon" [src]="avatar.img"/>
-<ng-template #icon>
-  <svg [ngClass]="{
-'icon': true,
-'circular': avatar.shape === 'circular',
-'rounded': avatar.shape === 'rounded',
-'xxs': avatar.size === 'xxs',
-'xs': avatar.size === 'xs',
-'sm': avatar.size === 'sm',
-'md': avatar.size === 'md',
-'lg': avatar.size === 'lg',
-'xl': avatar.size === 'xl'
-}" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-  </svg>
-</ng-template>
-<span *ngIf="avatar.badgeColor" class="avatar-notification"
-[ngClass]="{
-'circular-notification': avatar.shape === 'circular',
-'rounded-notification': avatar.shape === 'rounded',
-'gray': avatar.badgeColor === 'gray',
-'red': avatar.badgeColor === 'red',
-'green': avatar.badgeColor === 'green',
-'top-right': avatar.badgePosition === 'topRight',
-'bottom-right': avatar.badgePosition == 'bottomRight',
-'xxs': avatar.size === 'xxs',
-'xs': avatar.size === 'xs',
-'sm': avatar.size === 'sm',
-'md': avatar.size === 'md',
-'lg': avatar.size === 'lg',
-'xl': avatar.size === 'xl'
-}">
-</span>
-</bio-avatar>
-`,
+    template: `<bio-avatar [avatar]="avatar"></bio-avatar>`,
     props: {
         ...args
     }
 });
 
 export const Primary = Template.bind({});
+
 Primary.args = {
     avatar: {
-        shape: 'circular',
         size: 'xl',
-        badgeColor: 'gray',
+        badgeColor: 'green',
         badgePosition: 'topRight',
         // img: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        img: null
+        img: null,
+        placeholder: 'IO',
     },
 };
--- libs/avatar/src/lib/components/avatar/avatar.component.ts
+++ libs/avatar/src/lib/components/avatar/avatar.component.ts
@@ -20,12 +20,69 @@ export type Avatar = {
 export class BioAvatarComponent implements OnInit {
     @Input() avatar!: Avatar;
 
-    constructor() {
-        // demo
-    }
-
     ngOnInit(): void {
+        if (!this.avatar.shape || this.avatar.shape === null) {
+            this.avatar.shape = 'circular';
+        }
+
+        if (!this.avatar.size || this.avatar.size === null) {
+            this.avatar.size = 'md'
+        }
+
+        if (this.avatar.placeholder) {
+            delete this.avatar.img;
+        }
         console.log(this.avatar);
     }
 
+    get addPlaceHolderContainerClass() {
+        return {
+            'xxs': this.avatar.size === 'xxs',
+            'xs': this.avatar.size === 'xs',
+            'sm': this.avatar.size === 'sm',
+            'md': this.avatar.size === 'md',
+            'lg': this.avatar.size === 'lg',
+            'xl': this.avatar.size === 'xl'
+        }
+    }
+
+    get addPlaceHolderTextClass() {
+        return {
+            'xs': this.avatar.size === 'xs',
+            'sm': this.avatar.size === 'sm',
+            'lg': this.avatar.size === 'lg',
+            'xl': this.avatar.size === 'xl'
+        }
+    }
+
+    get addImageAndIconClass() {
+        return {
+            'circular': this.avatar.shape === 'circular',
+            'rounded': this.avatar.shape === 'rounded',
+            'xxs': this.avatar.size === 'xxs',
+            'xs': this.avatar.size === 'xs',
+            'sm': this.avatar.size === 'sm',
+            'md': this.avatar.size === 'md',
+            'lg': this.avatar.size === 'lg',
+            'xl': this.avatar.size === 'xl'
+        }
+    }
+
+    get badgeClass() {
+        return {
+            'circular-notification': this.avatar.shape === 'circular',
+            'rounded-notification': this.avatar.shape === 'rounded',
+            'gray': this.avatar.badgeColor === 'gray',
+            'red': this.avatar.badgeColor === 'red',
+            'green': this.avatar.badgeColor === 'green',
+            'top-right': this.avatar.badgePosition === 'topRight',
+            // 'bottom-right': this.avatar.badgePosition == 'bottomRight',
+            'xxs': this.avatar.size === 'xxs',
+            'xs': this.avatar.size === 'xs',
+            'sm': this.avatar.size === 'sm',
+            'md': this.avatar.size === 'md',
+            'lg': this.avatar.size === 'lg',
+            'xl': this.avatar.size === 'xl'
+        }
+    }
 }
