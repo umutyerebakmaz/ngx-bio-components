39f2d2810f40fecccbfd981e090aadeba6162d7d
umutyerebakmaz <umutyerebakmaz@gmail.com>
2023-02-24 15:08:32 +0300

    update libraryGenerator

--- tools/generators/generate-library/index.ts
+++ tools/generators/generate-library/index.ts
@@ -7,42 +7,42 @@ import { transformFile } from '../utilities/transform';
 
 export default async function (host: Tree, schema: Schema) {
 
-  await libraryGenerator(host, {
-    name: schema.name,
-    publishable: true,
-    importPath: `@ngx-bio-components/${schema.name}`,
-    style: 'scss',
-    prefix: 'bio',
-    strict: true,
-  } as Schema);
+    await libraryGenerator(host, {
+        name: schema.name,
+        publishable: true,
+        importPath: `@ngx-bio-components/${schema.name}`,
+        style: 'scss',
+        prefix: 'bio',
+        strict: true,
+    });
 
-  // get the expected Module name
-  const moduleName = names(schema.name).className + 'Module';
+    // get the expected Module name
+    const moduleName = names(schema.name).className + 'Module';
 
-  // prefix the module name
-  transformFile(host, `libs/${names(schema.name).fileName}/src/lib/${names(schema.name).fileName}.module.ts`, [
-    prefixIdentifierTransformer(moduleName),
-  ]);
+    // prefix the module name
+    transformFile(host, `libs/${names(schema.name).fileName}/src/lib/${names(schema.name).fileName}.module.ts`, [
+        prefixIdentifierTransformer(moduleName),
+    ]);
 
-  // Setup storybook
-  await storybookConfigurationGenerator(host, {
-    name: schema.name,
-    configureCypress: false,
-    generateCypressSpecs: false,
-    linter: Linter.EsLint,
-  } as any);
+    // Setup storybook
+    await storybookConfigurationGenerator(host, {
+        name: schema.name,
+        configureCypress: false,
+        generateCypressSpecs: false,
+        linter: Linter.EsLint,
+    } as any);
 
-  await formatFiles(host);
+    await formatFiles(host);
 
-  return () => {
-    installPackagesTask(host);
-  };
+    return () => {
+        installPackagesTask(host);
+    };
 }
 interface Schema {
-  name: string;
-  publishable: boolean;
-  importPath: string;
-  style: string;
+    name: string;
+    publishable: boolean;
+    importPath: string;
+    style: string;
 }
 
 
